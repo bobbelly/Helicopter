3035f47941810fa18b96b261d626f8693776676a
Matt Belley <mattbelley@gmail.com>
2017-04-30 22:14:45 -0600

    Much better quadcopter physics (pitch and force)

--- Helicopter/Assets/Scripts/QuadController.cs
+++ Helicopter/Assets/Scripts/QuadController.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class QuadController : MonoBehaviour {
 
 	private Rigidbody rb;
-	public float speed;
+	public float speed = 1f;
+	public float rotateSpeed = 1f;
+	//public float tilt;
+	// public float forcer;
 
 
 	// Use this for initialization
@@ -13,17 +16,34 @@ public class QuadController : MonoBehaviour {
 
 		rb = GetComponent<Rigidbody> ();
 
+
+
 	}
 
-	void FixedUpdate ()
-	{
+	void FixedUpdate ()	{
+
 		float moveHorizontalX = Input.GetAxis ("HorizontalX");
 		float moveHorizontalY = Input.GetAxis ("Throttle");
 		float moveVertical = Input.GetAxis ("HorizontalY");
+		moveHorizontalX *= speed;
+		moveHorizontalY *= speed;
+		moveVertical *= speed;
 
-		Vector3 movement = new Vector3 (moveHorizontalX, moveHorizontalY, moveVertical);
-		rb.velocity = movement * speed;
 
+		Vector3 rotateYaw = new Vector3(Input.GetAxis ("Yaw"),0f, 0f);
+		rotateYaw *= rotateSpeed;
+
+		rb.MovePosition (rb.position + (transform.forward * moveHorizontalX) * Time.deltaTime);
+		rb.MovePosition (rb.position + (transform.up * moveVertical) * Time.deltaTime);
+		rb.MovePosition (rb.position + (transform.right * moveHorizontalY) * Time.deltaTime);
+
+		//Vector3 movement = new Vector3 (moveHorizontalX, moveHorizontalY, moveVertical);
+		//rb.velocity = movement * speed;
+		// rb.rotation = Quaternion.Euler (rb.velocity.x, rb.velocity.y, rb.velocity.z);
+		rb.MoveRotation(transform.rotation * Quaternion.Euler(rotateYaw));
+		//rb.AddForce(movement * speed);
+
+		// print (rb.velocity);
 
 	}
 
---
+++ Helicopter/Assets/Scripts/QuadForceController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuadForceController : MonoBehaviour {
+
+	private Rigidbody rb;
+	public float speed = 1f;
+	public float torque = 1f;
+	public float rotateSpeed = 1f;
+	private float frontLeftForce = 0f;
+	private float frontRightForce = 0f;
+	private float rearLeftForce = 0f;
+	private float reafRightForce = 0f;
+
+	// Use this for initialization
+	void Start () {
+		
+		rb = GetComponent<Rigidbody> ();
+
+	}
+
+	void FixedUpdate (){
+
+		float moveHorizontalX = Input.GetAxis ("HorizontalX");
+		float moveHorizontalY = Input.GetAxis ("Throttle");
+		float moveVertical = Input.GetAxis ("HorizontalY");
+
+		rb.AddRelativeForce (Vector3.right * moveHorizontalY * speed, ForceMode.Impulse);
+
+		rb.AddRelativeTorque (Vector3.up * moveHorizontalX * torque, ForceMode.Impulse);
+		rb.AddRelativeTorque (Vector3.forward * moveVertical * torque, ForceMode.Impulse);
+
+		Vector3 rotateYaw = new Vector3(Input.GetAxis ("Yaw"),0f, 0f);
+		rotateYaw *= rotateSpeed;
+		rb.MoveRotation(transform.rotation * Quaternion.Euler(rotateYaw));
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+}
